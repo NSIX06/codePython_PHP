76e6e0ade982d7d4501ad1152bee6055cb6e6170
NSIX06 <felipebugalho2016@gmail.com>
2024-08-28 16:34:09 -0400

    tarefa_feita

--- Python/atividades_Portugol_&_Python/atividadePython.py
+++ Python/atividades_Portugol_&_Python/atividadePython.py
@@ -127,3 +127,148 @@ else:
 
 #* 10 - Atividade salário:
 
+"""
+salario = 0
+percentual = 0.15
+salario_final = 0
+
+salario = float(input("Digite o seu salário: "))
+
+salario_final = salario + (1.15 * salario)
+
+if salario < 1500:
+    print("\nVálido para aumento!")
+    print("\nO salário final é de: ", salario_final)
+elif salario >= 1500:
+    print("\nInvávildo para aumento!")
+else:
+    print("\nFim do programa")
+"""
+
+#* 11 - Atividade voto:
+
+"""
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 16:
+    print("Você está hapto para votar!")
+else:
+    print("Você não está hapto para votar!")
+"""
+
+#* 12 - Atividade IMC:
+"""
+
+peso = 0
+altura = 0
+imc = 0
+
+peso = float(input("Informe o seu peso(em kg): "))
+
+altura = float(input("Informe a sua altura: "))
+
+imc = peso / (altura* altura)
+
+print("Seu IMC é:", imc)
+
+if imc < 16:
+    print("\nSua classificação é Magreza grave")
+elif imc < 17:
+    print("\nSua classificação é Magreza moderada")
+elif imc < 18.5:
+    print("\nSua classificação é Magreza leve")
+elif imc < 25:
+    print("\nSua classificação é Saúdavel")
+elif imc < 30:
+    print("\nSua classificação é Sobrepeso")
+elif imc < 35:
+    print("\nObesidade Grau I")
+elif imc < 40:
+    print("\nObesidade Grau II")
+else:
+    print("\nObesidade Grau III (mórbida)")
+"""
+
+
+#* 13 - Atividade dias da semana:
+"""
+dia = int(input("Digite um número de 1 à 7: "))
+
+dias_da_semana = {
+    1: "Domingo",
+    2: "Segunda-Feira",
+    3: "Terça-Feira",
+    4: "Quarta-Feira",
+    5: "Quinta-Feira",
+    6: "Sexta-Feira",
+    7: "Sábado"
+}
+
+print(dias_da_semana.get(dia, "Número inválido! Digite um número de 1 à 7."))
+"""
+
+#* 14 - Atividade de notas:
+"""
+nota = int(input("Digite uma nota de 0 à 10: "))
+
+notas = {
+    "Excelente" :(9,10),
+    "Bom":(7,8),
+    "Regular":(5,6),
+    "Insufiente": (0,4)
+
+}
+
+if nota == 10:
+    notas = "Excelente"
+elif nota >= 7:
+    notas = "Bom"
+elif nota >= 5:
+    notas = "Regular"
+else:
+    notas = "Insufuciente"
+
+print(f"Nota: {notas}")
+"""
+
+#* 15 - Atividade planetas:
+"""
+
+planeta = int(input("Digite um número de 1 à 8: "))
+
+planetas = {
+    1: "Mercúrio",
+    2: "Vênus",
+    3: "Terra",
+    4: "Marte",
+    5: "Júpiter",
+    6: "Saturno",
+    7: "Urano",
+    8: "Netuno"
+}
+
+print(planetas.get(planeta, "Número inválido! Digite um número de 1 à 7."))
+"""
+
+#* 16 - Atividade meses:
+
+"""
+mes = int(input("Digite um número de 1 à 12: "))
+
+meses = {
+    1: "Janeiro",
+    2: "Fevereiro",
+    3: "Março",
+    4: "Abril",
+    5: "Maio",
+    6: "Junho",
+    7: "Julho",
+    8: "Agosto",
+    9: "Setembro",
+    10: "Outubro",
+    11: "Novembro",
+    12: "Dezembro"
+}
+
+print(meses.get(mes, "Número inválido! Digite um número de 1 à 12."))
+"""
