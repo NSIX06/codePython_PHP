d743973df76c1f618bb3168d34f389bf6a8c7d99
NSIX06 <felipebugalho2016@gmail.com>
2024-09-10 21:35:46 -0400

    atvidade

--- Python/loopsPython/for.py
+++ Python/loopsPython/for.py
@@ -447,6 +447,7 @@ print(calcular_media(notas2))
 
 #?Lista de números pares
 
+'''
 def filtro_numeros(numeros):
     return [numero for numero in numeros if numero % 2 ==0]
 
@@ -455,3 +456,122 @@ numeros2 = [7, 18, 21, 24, 29]
 
 print(filtro_numeros(numeros1))
 print(filtro_numeros(numeros2))
+'''
+
+
+#*Você foi contratado para desenvolver uma solução simples para uma loja. A loja está realizando uma promoção em que todos os produtos terão um desconto de 10% aplicado no preço original.
+#*Sua tarefa é criar um pequeno programa que automatize esse processo, aplicando o desconto automaticamente para todos os produtos da loja. A lista de preços dos produtos já está disponível, 
+#*e você precisa aplicar o desconto de forma eficiente, usando as ferramentas de programação map e função.
+
+#*Seu objetivo é garantir que todos os preços atualizados com desconto sejam exibidos corretamente para os clientes da loja.
+
+
+precos = [299, 200, 499, 300, 400, 559, 1500]  
+
+desconto = list(map(lambda x: x  - (x * 0.1), precos))
+print(desconto)
+
+#*Você terá que desenvolver um sistema de controle de entrada em uma boate chamada "Boate Azul". Para garantir que a boate siga as regras, apenas pessoas com 18 anos ou mais podem entrar.
+#*Sua tarefa é criar um programa que receba a idade de 10 pessoas que estão na fila para entrar na boate. Depois disso, o programa deve verificar, de forma automática, 
+#*quem tem permissão para entrar, utilizando as ferramentas list, filter e função.
+#*O objetivo é garantir que somente as pessoas com a idade mínima exigida possam entrar, e o sistema deve exibir a lista de idades que foram autorizadas a entrar na boate.
+
+'''
+idades = [18, 25, 36, 22, 21, 24, 17, 30, 32, 31, 28, 29, 16, 15, 54]
+idades_maiores = []  
+
+idades_maiores = list(filter(lambda x: x > 18, idades))
+print(idades_maiores)
+'''
+'''
+def verficar_idades(idades):
+    if idades >= 18:
+        return True
+    else:
+        return False;
+    
+idades_maiores = list(filter(verficar_idades, idades))
+print(idades_maiores)
+'''
+
+#*O setor de recursos humanos de uma empresa te chamou para ajudar na automatização do cálculo de impostos sobre os salários dos funcionários. A empresa precisa calcular 5% de imposto de IR e 4% de imposto de INSS sobre cada salário e 
+#*quer que você desenvolva um programa para realizar essa tarefa de forma automática.
+#*Sua tarefa é criar um programa que receba a lista de salários dos funcionários e calcule o valor do imposto a ser descontado de cada um. Para isso, utilize map, list, e função.
+#*Ao final, o programa deve exibir os valores de impostos que serão descontados para cada funcionário.
+'''
+salarios = [2000, 6874, 3000, 6200, 5000, 8000, 5550, 7500, 2500, 1489]  
+imposto_desconto = []
+
+IR = 0.04
+INSS = 0.05
+
+imposto_desconto = list(map(lambda x: x - (x * (IR + INSS)), salarios))
+print(imposto_desconto)
+
+#*Uma escola precisa de ajuda para calcular a média das notas de três alunos. Cada aluno fez duas provas, e para ser aprovado, precisa de uma média igual ou maior que 6.
+#*Sua tarefa é criar um programa que receba as notas desses alunos, calcule a média de cada um e mostre se foram aprovados ou reprovados.
+#*Para isso use os conceitos de matriz.
+#*Notas de 3 alunos em 2 provas  
+'''
+'''
+notas = [ 
+
+[5, 7], # Aluno 1
+
+[6, 4], # Aluno 2
+
+[8, 9] # Aluno 3
+
+] 
+
+for i in range(len(notas)): 
+    for j in range(len(notas[i])):
+        if notas[i] [j] >= 6  :
+            aprovado = True
+            print("Aprovado")
+            break
+
+if not aprovado:
+    print("Reprovado.") 
+
+'''
+
+#*Você está desenvolvendo um sistema para uma loja de informática que precisa aplicar um desconto de 20% em todos os preços de produtos acima de R$100,00. 
+#*Sua tarefa é criar um programa que, com base em uma lista de preços, aplique o desconto apenas aos produtos que custam mais de R$100,00 e mantenha os outros preços inalterados.
+#*Seu programa deve usar uma função para calcular o desconto e os conceitos de "list comprehension" para aplicar essa função.
+'''
+produtos = [200, 300, 100, 400, 90]
+
+desconto = [produtos - (produtos * 0.2) for produtos in produtos if produtos > 100]
+
+
+print(desconto)
+
+
+'''
+'''
+[x for x in produtos if 100 in x ]
+'''
+
+'''
+
+'''
+'''
+media_notas = []
+
+for i in range (3):
+    aluno_notas = []
+    print(f"Digite as notas dos alunos {i+1}:")
+    notas = 0
+    for j in range (2):
+        nota = float(input(f"Nota {j+1}:"))
+        notas+= nota
+        aluno_notas.append(nota)
+        media = notas/2
+        media_notas.append(media)
+for x in media_notas:
+    if x >= 6:
+        print(f"A media do aluno {i+1} é {x: .2f}, ou seja, ele esta aprovado!")
+    else:
+        print(f"A media do aluno {i+1} é {x: .2f}, ou seja, ele esta reprovado!")
+'''
